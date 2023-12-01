bc7b7c4bcc2481a2d8070448d9a0d865c59dc87e
Tiago Bovolin <tiagaoalb@gmail.com>
2023-11-30 21:40:35 -0300

    chore: remove useless equals and hashcode

--- src/main/java/com/peti9engineeringlab/dto/TutorDTO.java
+++ src/main/java/com/peti9engineeringlab/dto/TutorDTO.java
@@ -2,7 +2,6 @@ package com.peti9engineeringlab.dto;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 
 public record TutorDTO(
         Long id,
@@ -11,18 +10,4 @@ public record TutorDTO(
         LocalDate birthDate,
         List<PetDTO> pets
 ) {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TutorDTO tutorDTO = (TutorDTO) o;
-        return Objects.equals(name, tutorDTO.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-
 }

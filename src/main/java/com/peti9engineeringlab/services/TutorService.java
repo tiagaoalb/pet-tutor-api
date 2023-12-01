6c1c0f1ce07d773a1b3f20e6908b96df726626bd
Tiago <tiagaoalb@gmail.com>
2023-12-01 16:22:38 -0300

    chore: add @Transactional annotation o methods

--- src/main/java/com/peti9engineeringlab/services/TutorService.java
+++ src/main/java/com/peti9engineeringlab/services/TutorService.java
@@ -3,7 +3,7 @@ package com.peti9engineeringlab.services;
 import com.peti9engineeringlab.dto.TutorDTO;
 import com.peti9engineeringlab.model.Tutor;
 import com.peti9engineeringlab.repositories.TutorRepository;
-import com.peti9engineeringlab.services.exceptions.DataInfoException;
+import com.peti9engineeringlab.services.exceptions.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -21,27 +21,30 @@ public class TutorService {
     public TutorDTO createTutor(TutorDTO tutorDTO) {
         var tutorName = tutorDTO.name();
         if (tutorRepository.existsByName(tutorName)) {
-            throw new DataInfoException(HttpStatus.CONFLICT, "Tutor with name " + tutorName + " already exists.");
+            throw new DataNotFoundException(HttpStatus.CONFLICT, "Tutor with name " + tutorName + " already exists.");
         }
         var newTutor = new Tutor(tutorDTO);
         var savedTutor = tutorRepository.save(newTutor);
         return savedTutor.toTutorDTO();
     }
 
+    @Transactional
     public List<TutorDTO> findAll() {
         var tutors = tutorRepository.findAll();
         return tutors.stream().map(Tutor::toTutorDTO).toList();
     }
 
+    @Transactional
     public TutorDTO findById(Long id) {
-        var tutor = tutorRepository.findById(id).orElseThrow(() -> new DataInfoException(HttpStatus.NOT_FOUND, "Tutor with id " + id + " was not found"));
+        var tutor = tutorRepository.findById(id).orElseThrow(() -> new DataNotFoundException(HttpStatus.NOT_FOUND, "Tutor with id " + id + " was not found"));
         return tutor.toTutorDTO();
     }
 
+    @Transactional
     public List<TutorDTO> findTutorByName(String name) {
         var tutors = tutorRepository.findByNameContainingIgnoreCase(name);
         if (tutors.isEmpty()) {
-            throw new DataInfoException(HttpStatus.NOT_FOUND, "Tutor with name " + name + " was not found");
+            throw new DataNotFoundException(HttpStatus.NOT_FOUND, "Tutor with name " + name + " was not found");
         }
         return tutors.stream().map(Tutor::toTutorDTO).toList();
     }

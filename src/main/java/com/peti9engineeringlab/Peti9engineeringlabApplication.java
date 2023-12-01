424bbec1f0931cf61ba3ac34e07cfdb36328180a
Tiago Bovolin <tiagaoalb@gmail.com>
2023-11-30 21:39:25 -0300

    chore: remove empty space

--- src/main/java/com/peti9engineeringlab/Peti9engineeringlabApplication.java
+++ src/main/java/com/peti9engineeringlab/Peti9engineeringlabApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Peti9engineeringlabApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Peti9engineeringlabApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Peti9engineeringlabApplication.class, args);
+    }
 }

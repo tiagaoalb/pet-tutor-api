9b4bbbd1a40851adc091380041fc710586e90f47
Tiago Bovolin <tiagaoalb@gmail.com>
2023-11-30 21:39:09 -0300

    chore: change @Data to @Getter and @Setter + hashcode and equals

--- src/main/java/com/peti9engineeringlab/model/Pet.java
+++ src/main/java/com/peti9engineeringlab/model/Pet.java
@@ -4,50 +4,38 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.peti9engineeringlab.dto.PetDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_pets")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Pet {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank
     @EqualsAndHashCode.Include
     private String name;
-
     @Column(name = "pet_breed")
     private String petBreed;
-
     @Column(name = "birth_date")
     private LocalDate birthDate;
-
     private String color;
-
     private Double weight;
-
     @Column(name = "vaccine_date")
     private LocalDate vaccineDate;
-
     @Column(name = "vaccine_type")
     private String vaccineType;
-
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "tutor_id")
     private Tutor tutor;
-
     public Pet (PetDTO dto) {
-
         this.id = dto.id();
         this.name = dto.name();
         this.petBreed = dto.petBreed();
@@ -56,12 +44,10 @@ public class Pet {
         this.weight = dto.weight();
         this.vaccineDate = dto.vaccineDate();
         this.vaccineType = dto.vaccineType();
-
         if (dto.tutorName() != null) {
             this.tutor = new Tutor(dto.tutorName());
         }
     }
-
     public PetDTO toPetDTO() {
         return new PetDTO(
             this.getId(),
@@ -76,4 +62,16 @@ public class Pet {
         );
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pet pet = (Pet) o;
+        return Objects.equals(tutor, pet.tutor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tutor);
+    }
 }
--- src/main/java/com/peti9engineeringlab/model/Tutor.java
+++ src/main/java/com/peti9engineeringlab/model/Tutor.java
@@ -4,54 +4,44 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.peti9engineeringlab.dto.PetDTO;
 import com.peti9engineeringlab.dto.TutorDTO;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_tutors")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Tutor {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique = true)
     @EqualsAndHashCode.Include
     private String name;
-
     private String nickName;
-
     @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate birthDate;
-
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "tutor", cascade = CascadeType.ALL)
     private List<Pet> pets = new ArrayList<>();
-
     public Tutor(TutorDTO dto) {
         this.id = dto.id();
         this.name = dto.name();
         this.nickName = dto.nickName();
         this.birthDate = dto.birthDate();
-
         dto.pets().forEach(p -> {
             Pet pet = new Pet(p);
             pet.setTutor(this);
             pets.add(pet);
         });
     }
-
     public Tutor(String s) {
         this.name = s;
     }
-
     public TutorDTO toTutorDTO() {
         List<PetDTO> petsDTO = new ArrayList<>();
         this.pets.forEach(p -> {
@@ -71,4 +61,16 @@ public class Tutor {
         return new TutorDTO(this.id, this.name, this.nickName, this.birthDate, petsDTO);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tutor tutor = (Tutor) o;
+        return Objects.equals(name, tutor.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
